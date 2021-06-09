fb7620072704e6a67945606afb0fb39a1a932d00
imamzz <kzuto24@gmail.com>
2021-06-09 22:24:28 +0700

    Volley

---
+++ Volley/app/src/main/java/com/example/volley/Util/ServerAPI.java
@@ -0,0 +1,8 @@
+package com.example.volley.Util;
+
+public class ServerAPI {
+    public static final String URL_DATA = "http://192.168.1.4/minggu_11/Mahasiswa/Api";
+    public static final String URL_INSERT = "http://192.168.1.4/minggu_11/Mahasiswa/ApiInsert";
+    public static final String URL_DELETE = "http://192.168.1.4/minggu_11/Mahasiswa/ApiDelete";
+    public static final String URL_UPDATE = "http://192.168.1.4/minggu_11/Mahasiswa/ApiUpdate";
+}
